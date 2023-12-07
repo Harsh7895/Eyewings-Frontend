020213936137104f30edc2ffda503188b4d9f011
Harsh Pawar <harshsik7890@gmail.com>
2023-12-08 00:31:13 +0530

    Update Choice.jsx

--- src/components/frameChoice/Choice.jsx
+++ src/components/frameChoice/Choice.jsx
@@ -43,7 +43,7 @@ const Card = ({image , title, type})=>{
         <div className="card">
             <img src={image} alt="" />
             <h3>{title}</h3>
-            <Link to="/product/listing/all" >Explore</Link>
+            <Link to={'product/all/listing'} >Explore</Link>
         </div>
     )
 }
